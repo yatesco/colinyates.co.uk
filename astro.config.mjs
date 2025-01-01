9babc475273408585987ef41ad4ce9155296fa42
Colin Yates <colin@colinyates.co.uk>
2025-01-01 12:57:23 +0000

    tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,8 +5,10 @@ import preact from "@astrojs/preact";
 import icon from "astro-icon";
 import sitemap from "@astrojs/sitemap";
 
+import tailwind from "@astrojs/tailwind";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://colinyates.co.uk",
-  integrations: [preact(), icon(), sitemap()],
+  integrations: [preact(), icon(), sitemap(), tailwind()],
 });
