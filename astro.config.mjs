28f2124df41fedf9a51f24ba6645dad9ff7fbea6
Colin Yates <colin@colinyates.co.uk>
2025-01-01 16:59:53 +0000

    closes #16 - removed preact

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,11 @@
 import { defineConfig } from "astro/config";
 
-import preact from "@astrojs/preact";
-
 import icon from "astro-icon";
 import sitemap from "@astrojs/sitemap";
-
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://colinyates.co.uk",
-  integrations: [preact(), icon(), sitemap(), tailwind()],
+  integrations: [icon(), sitemap(), tailwind()],
 });
