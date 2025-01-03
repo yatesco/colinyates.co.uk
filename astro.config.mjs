eb3a87f9c3b282bc9692849e05e7a5ac98f34cdc
Colin Yates <colin@colinyates.co.uk>
2025-01-03 17:39:38 +0000

    closes #18 - wide pages wrap and styling is more consistent

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,4 +8,16 @@ import tailwind from "@astrojs/tailwind";
 export default defineConfig({
   site: "https://colinyates.co.uk",
   integrations: [icon(), sitemap(), tailwind()],
+  markdown: {
+    shikiConfig: {
+      themes: {
+        light: "catppuccin-latte",
+        dark: "github-dark",
+      },
+      // unfortunately this isn't supported yet so it is done in global.css...
+      // lineNumbers: true,
+      // Enable word wrap to prevent horizontal scrolling
+      wrap: true,
+    },
+  },
 });
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      // typography: {
+      //   DEFAULT: {
+      //     css: {
+      //       pre: {
+      //         // otherwise shiki doesn't wrap the code and max-w-none takes things off the right hand side of the screen
+      //         "white-space": "pre-wrap",
+      //       },
+      //     },
+      //   },
+      // },
+    },
+  },
+  plugins: [require("@tailwindcss/typography")],
+};
